ecd838f410f4ff77e0d451b3beb4568041bb232c
vesakhaa <anandavesakha2905@gmail.com>
2025-10-08 08:28:52 +0700

    Percobaan 1

---
+++ jobSheet05/cetakKRS04.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class cetakKRS04 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("--- Cetak KRS SIAKAD ---");
+        System.out.print("Apakah UKT sudah lunas? (true/false): ");
+        boolean uktLunas = sc.nextBoolean();
+
+        if (uktLunas){
+            System.out.println("Pembayaran UKT Terverifikasi");
+            System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
+        } else{
+            System.out.println("Silahkan lakukan pembayaran UKT terlebih dahulu");
+        }
+       
+    }
+}
