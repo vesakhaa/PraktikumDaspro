0537d3112387aee0e5cde2cd2b0df9f48bf58c81
vesakhaa <anandavesakha2905@gmail.com>
2025-10-22 10:19:57 +0700

    Commit latihan rental mobil

---
+++ rentMobilLat3.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class rentMobilLat3 {
+    public static void main(String[] args) {
+       double biayaSewa = 300000;
+        double biayaSupir = 200000;
+        double BBM = 1000;
+        double totalBiaya;
+
+        Scanner sc = new Scanner(System.in);
+
+        //menghitung biaya sewa dan supir
+        double biayaMobil = biayaSewa + biayaSupir;
+        
+
+        //input lama sewa
+        System.out.println("Masukkan lama sewa: ");
+        double lamaSewa = sc.nextDouble();
+
+        //input jarak tempuh
+        System.out.println("Masukkan jarak tempuh: ");
+        double jarakTempuh = sc.nextDouble();
+
+        //menghitung biaya bbm
+        double biayaBBM = BBM * jarakTempuh;
+
+        if(lamaSewa >=0 && lamaSewa<=30){
+            totalBiaya = biayaMobil*lamaSewa + biayaBBM;
+            System.out.println("Biaya yang harus anda bayar adalah: Rp "+ totalBiaya);
+        }else{
+            System.out.println("Lama sewa tidak boleh lebih dari 30 hari");
+        }
+    }
+}
---
+++ rentMobilLat4.java
@@ -0,0 +1,49 @@
+
+import java.util.Scanner;
+
+public class rentMobilLat4 {
+
+    public static void main(String[] args) {
+        double biayaSewa = 300000;
+        double biayaSupir = 200000;
+        double pertalite = 1000;
+        double pertamax = 1300;
+        String bbm;
+        Double biayaBBM = null;
+
+        Scanner sc = new Scanner(System.in);
+
+        //menghitung biaya sewa dan supir
+        double biayaMobil = biayaSewa + biayaSupir;
+
+        System.out.println("Masukkan jenis bbm(pertalite/pertamax): ");
+        bbm = sc.next();
+
+        //input jarak tempuh
+        System.out.println("Masukkan jarak tempuh: ");
+        double jarakTempuh = sc.nextDouble();
+
+        //menghitung biaya bbm
+        if (bbm.equalsIgnoreCase("pertalite")) {
+            biayaBBM = jarakTempuh * pertalite;
+        } else if (bbm.equalsIgnoreCase("pertamax")) {
+            biayaBBM = jarakTempuh * pertamax;
+        }
+
+        //input lama sewa
+        System.out.println("Masukkan lama sewa: ");
+        double lamaSewa = sc.nextDouble();
+
+        if (lamaSewa >= 0 && lamaSewa <= 30) {
+            double totalBiaya = biayaMobil * lamaSewa + biayaBBM;
+
+            if (totalBiaya >= 2000000) {
+                double diskon = totalBiaya * 0.05;
+                totalBiaya = totalBiaya - diskon;
+            }
+            System.out.println("Biaya yang harus anda bayar adalah: Rp " + totalBiaya);
+        } else {
+            System.out.println("Lama sewa tidak boleh lebih dari 30 hari");
+        }
+    }
+}
