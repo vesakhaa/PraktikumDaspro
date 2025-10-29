0dae0b5f8b87b6679ebf54822bfe4abe2662f41c
vesakhaa <anandavesakha2905@gmail.com>
2025-10-29 09:05:20 +0700

    setelah modifikasi

--- jobDheet07/siakadFor04.java
+++ jobDheet07/siakadFor04.java
@@ -1,8 +1,10 @@
 import java.util.Scanner;
 public class siakadFor04 {
     public static void main(String[] args) {
-        double nilai, tertinggi = 0, terendah = 100;
+        double nilai, tertinggi = 0, terendah = 100, kkm=60;
+        int lulus = 0, tidakLulus = 0;
         Scanner input = new Scanner(System.in);
+        
 
         for (int i=1; i<=10; i++){
             System.out.println("Masukkan nilai mahasiswa ke-" + i + ":");
@@ -13,8 +15,15 @@ public class siakadFor04 {
             if(nilai < terendah){
                 terendah=nilai;
             }
+            if(nilai >= kkm){
+                lulus++;
+            }else{
+                tidakLulus++;
+            }
         }
         System.out.println("Nilai tertinggi: " + tertinggi);
         System.out.println("Nilai tertinggi: " + terendah);
+        System.out.println("Jumlah siswa lulus: " + lulus);
+        System.out.println("Jumlah siswa tidak lulus: " + tidakLulus);
     }
 }
