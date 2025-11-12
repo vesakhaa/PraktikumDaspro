567a7752fdfb2a64738ed016dc70e1d2bcc71582
vesakhaa <anandavesakha2905@gmail.com>
2025-11-12 11:16:29 +0700

    First Commit

---
+++ jobSheet09/ArrayNilai04.java
@@ -0,0 +1,19 @@
+package jobSheet09;
+
+import java.util.Scanner;
+
+public class ArrayNilai04 {
+    public static void main(String[] args) {
+        Scanner input= new Scanner(System.in);
+
+        int[] nilaiAkhir = new  int[10];
+
+        for(int i=0; i<10;i++){
+            System.out.print("Masukkan nilai akhir ke-"+i+" : ");
+            nilaiAkhir[i] = input.nextInt();
+        }
+        for(int i=0;i<10;i++){
+            System.out.println("Nilai akhir ke-"+i+" adalah " +nilaiAkhir[i]);
+        }
+    }
+}
