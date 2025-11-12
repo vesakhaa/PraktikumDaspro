4eff090e72c5bfc1f4e294b5a08223647dffe5f3
vesakhaa <anandavesakha2905@gmail.com>
2025-11-12 10:50:08 +0700

    Second Commit(setelah modif)

--- jobSheet09/ArrayBilangan04.java
+++ jobSheet09/ArrayBilangan04.java
@@ -2,16 +2,10 @@ package jobSheet09;
 
 public class ArrayBilangan04 {
     public static void main(String[] args) {
-        int[] bil = new  int [4];
+        int[] bil = {5, 13, -7, 17};
 
-        bil [0] = 5;
-        bil [1] = 13;
-        bil [2] = -7;
-        bil [3] = 17;
-
-        System.out.println(bil[0]);
-        System.out.println(bil[1]);
-        System.out.println(bil[2]);
-        System.out.println(bil[3]);
+        for (int i = 0; i < 4; i++){ 
+            System.out.println(bil[i]); 
+        }
     }
 }
