cbdc1cd6fcfe2e004abeffeacee86bb73b33b952
vesakhaa <anandavesakha2905@gmail.com>
2025-11-17 07:53:16 +0700

    Second commit

--- jobSheet09/ArrayRataNilai.java
+++ jobSheet09/ArrayRataNilai.java
@@ -8,6 +8,7 @@ public class ArrayRataNilai {
         int[] nilaiMhs=new int[10];
         double total=0;
         double rata2;
+        int jmlLulus = 0;
 
         for(int i = 0; i < nilaiMhs.length; i++){
             System.out.print("Masukkan nilai mahasiswa ke-"+i+" : ");
@@ -15,9 +16,13 @@ public class ArrayRataNilai {
         }
         for(int i = 0; i < nilaiMhs.length; i ++){
             total += nilaiMhs[i];
+            if(nilaiMhs[i]>=70){
+                jmlLulus++;
+            }
 
         }
         rata2 = total/nilaiMhs.length;
         System.out.println("Rata-rata nilai = "+rata2);
+        System.out.println("Jumlah mahasiswa yang lulus dengan nilai >70 adalah: "+jmlLulus);
     }
 }
