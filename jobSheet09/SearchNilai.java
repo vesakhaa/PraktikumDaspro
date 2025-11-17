37dfe0141488b1cc621cceb8e76495f09faf81cc
vesakhaa <anandavesakha2905@gmail.com>
2025-11-17 08:40:51 +0700

    second commit

--- jobSheet09/SearchNilai.java
+++ jobSheet09/SearchNilai.java
@@ -1,20 +1,39 @@
 package jobSheet09;
 
+import java.util.Scanner;
+
 public class SearchNilai {
     public static void main(String[] args) {
-        int[] arrNilai = {80, 85, 78, 96, 92, 86};
-        int key = 90;
-        int hasil = 0;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan banyaknya nilai yang akan diinput: ");
+        int jum = input.nextInt();
+
+        int[] arrNilai = new int[jum];
+
+        for (int i = 0; i < arrNilai.length; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + " : ");
+            arrNilai[i] = input.nextInt();
+        }
+
+        System.out.print("Masukkan nilai yang ingin dicari: ");
+        int key = input.nextInt();
+
+        int hasil = -1;
 
         for (int i = 0; i < arrNilai.length; i++) {
             if (key == arrNilai[i]) {
-                hasil = i;
-                break;
+                hasil = i; 
+                break;     
             }
         }
-        System.out.println();
-        System.out.println("Nilai " + key + " ketemu di indeks ke-" + hasil);
-        System.out.println();
 
+        System.out.println();
+        if (hasil != -1) {
+            System.out.println("Nilai " + key + " ketemu, merupakan nilai mahasiswa ke-" + (hasil+1));
+        } else {
+            System.out.println("Nilai " + key + " tidak ditemukan dalam array.");
+        }
+        System.out.println();
     }
 }
