be493465108abc476351a4262036cccc50e5f57a
vesakhaa <anandavesakha2905@gmail.com>
2025-11-17 08:30:30 +0700

    first commit

---
+++ jobSheet09/SearchNilai.java
@@ -0,0 +1,20 @@
+package jobSheet09;
+
+public class SearchNilai {
+    public static void main(String[] args) {
+        int[] arrNilai = {80, 85, 78, 96, 92, 86};
+        int key = 90;
+        int hasil = 0;
+
+        for (int i = 0; i < arrNilai.length; i++) {
+            if (key == arrNilai[i]) {
+                hasil = i;
+                break;
+            }
+        }
+        System.out.println();
+        System.out.println("Nilai " + key + " ketemu di indeks ke-" + hasil);
+        System.out.println();
+
+    }
+}
