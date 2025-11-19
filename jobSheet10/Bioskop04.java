2ce3b76774d13474196ea33dc0a215f971c50513
vesakhaa <anandavesakha2905@gmail.com>
2025-11-19 09:04:27 +0700

    third commit

--- jobSheet10/Bioskop04.java
+++ jobSheet10/Bioskop04.java
@@ -19,8 +19,15 @@ public class Bioskop04 {
         System.out.printf("%s \t %s \n", penonton[3][0], penonton[3][1] );
 
         System.out.println(penonton.length);
-        for(int i = 0;i < penonton.length; i ++){
-            System.out.println("Panjang baris ke-"+ (i+1) + ": " + penonton[i].length);
+
+        for (String[] barisPenonton : penonton) {
+            System.out.println("Panjang baris: "+ barisPenonton.length);            
         }
-    }
+
+        System.out.println("Penonton pada baris ke-3: ");
+
+        for(int i =0; i < penonton[2].length; i++){
+            System.out.println(penonton[2][i]);
+        }
+    }  
 }
