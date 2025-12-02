ff1712b9e9b88917e33b838cd61e45e829bf86a7
vesakhaa <anandavesakha2905@gmail.com>
2025-12-02 10:52:28 +0700

    First Commit

--- jobSheet12/PengunjungCafe04.java
+++ jobSheet12/PengunjungCafe04.java
@@ -2,11 +2,15 @@ package jobSheet12;
 
 public class PengunjungCafe04 {
     public static void main(String[] args) {
-        
+        daftarPengunjung("Ali", "Budi", "Citra");
+        daftarPengunjung("Andi");
+        daftarPengunjung("Doni", "Eti", "Fahmi", "Galih");
     }
 
     static void daftarPengunjung(String... namaPengunjung){
         System.out.println("Dasftar nama pengunjung: ");
-        for(int)
+        for(int i = 0; i<namaPengunjung.length; i++){
+            System.out.println("- "+namaPengunjung[i]);
+        }
     }
 }
